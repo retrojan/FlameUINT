7d878a6e1920382efe855d7e1a6b0bb263cf00c3
ReTrojan <retrojan31@gmail.com>
2025-05-16 18:35:24 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,33 @@
+--[[
+  Универсальный Loader для Roblox
+  Загружает разные скрипты в зависимости от ID игры
+]]
 
+local currentGameId = game.PlaceId
+
+-- Таблица соответствия ID игр и URL скриптов
+local gameScripts = {
+    [18550498098] = "https://raw.githubusercontent.com/retrojan/FlameUINT/refs/heads/main/guidefight.lua",
+}
+
+-- Проверяем, есть ли скрипт для текущей игры
+if gameScripts[currentGameId] then
+    local scriptUrl = gameScripts[currentGameId]
+    
+    print("Игра найдена в базе (ID:", currentGameId..")")
+    print("Загружаем скрипт:", scriptUrl)
+    
+    -- Пытаемся загрузить и выполнить скрипт
+    local success, errorMsg = pcall(function()
+        local scriptContent = game:HttpGet(scriptUrl, true)
+        loadstring(scriptContent)()
+    end)
+    
+    if not success then
+        warn("Ошибка загрузки скрипта:", errorMsg)
+    end
+else
+    print("Для этой игры нет скрипта (ID:", currentGameId..")")
+end
+
+print("Loader завершил работу")

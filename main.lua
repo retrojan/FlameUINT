7bf919ad7cf963bbabb8c208d4f24ede2e55f3aa
ReTrojan <retrojan31@gmail.com>
2025-09-06 17:11:40 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -29,11 +29,11 @@ FFFFFFFFFFF           llllllll  aaaaaaaaaa  aaaammmmmm   mmmmmm   mmmmmm    eeee
 local gameScripts = {
     [6403373529] = "https://raw.githubusercontent.com/retrojan/FlameUINT/refs/heads/main/SB.lua",
     [9015014224] = "https://raw.githubusercontent.com/retrojan/FlameUINT/refs/heads/main/SB.lua",
-
+    [124596094333302] = "https://raw.githubusercontent.com/retrojan/FlameUINT/refs/heads/main/SB.lua", -- FIXED
       
     --[18550498098] = "https://raw.githubusercontent.com/retrojan/FlameUINT/refs/heads/main/guidefight.lua", --Guide Boss Fight REMOVED: TEMPORARILY REMOVED: REWORK
     --[98726100529621] = "https://raw.githubusercontent.com/retrojan/FlameUINT/refs/heads/main/hexa.lua", -- Hexa REMOVED: TEMPORARILY REMOVED: REWORK
-    --[124596094333302] = "https://raw.githubusercontent.com/retrojan/FlameUINT/refs/heads/main/slapbattles.lua" -- New Players. REMOVED: HAVE BUGS AND PROBLEMS
+
 }
 
 
